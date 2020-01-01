b63d32922023f1db88d7aef3713c8036afb45197
Atabek <atabekmur@gmail.com>
2020-01-01 11:43:26 +1100

    Use shared code from Android

--- app/src/main/java/com/example/mpp/noty/MainActivity.kt
+++ app/src/main/java/com/example/mpp/noty/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.mpp.noty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mpp.noty.shared.createApplicationScreenMessage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_text.text = createApplicationScreenMessage()
     }
 }
